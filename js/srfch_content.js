f8596cc24af0f19559d63974ba82d3ed46392bbf
kaespi <kg@kgmw.ch>
2017-11-11 18:17:23 +0100

    add special banner for srf.ch/play pages

--- js/srfch_content.js
+++ js/srfch_content.js
@@ -1,6 +1,22 @@
 
 var contentLoaded = 0;
 
+// initialize the data for this tab (and remove the pageAction popup initially)
+if (!contentLoaded)
+{
+    // only send this message once (otherwise already existing data could be
+    // reset to zero)
+    contentLoaded = 1;
+    browser.runtime.sendMessage({});
+}
+
+if (document.documentURI
+    && document.documentURI.match(/srf\.ch\/play\/.*\/video\/.*id=[-a-zA-Z0-9]{36}/g))
+{
+    addSrfPlayBanner();
+}
+
+
 document.addEventListener("load", function(event) {
     // send an empty message to the background script. This makes the toolbar-popup
     // being disabled (by default) and the context menu being removed (if it exists)
@@ -76,6 +92,96 @@ window.addEventListener("mousedown", function(event) {
         // if the string with the ID was set then we can forward this information (via a
         // message) to the background script which can then do the actual extraction of
         // the URLs
+        console.log("content: send id: "+idStr);
         browser.runtime.sendMessage({srfchId: idStr});
     }
 }, true); 
+
+/*
+    addSrfPlayBanner():
+    On some srf.ch pages (in particular srf.ch/play/...) the ID of the video is directly
+    accessible in the address of the page. However, to let the add-on work also in this
+    case a banner needs to be added such that the "normal" functions can be used
+*/
+function addSrfPlayBanner()
+{
+    // check if this event has already fired and there's already a download link
+    // node added to the page.
+    var downloadUrlFound = 0;
+    if (document.getElementsByClassName("srfchaddon").length > 0)
+    {
+        downloadUrlFound = 1;
+    }
+    
+    // current tab seems to be a srf.ch page
+    // try to find out if it's really an SRF-play page...
+    var isSrfPlay = 0;
+    var metaElements = document.getElementsByTagName("meta");
+    if (!downloadUrlFound && metaElements && metaElements.length > 0)
+    {
+        for (var i=0; i<metaElements.length; i++)
+        {
+            var siteName = 0;
+            var content = '';
+            for (var k=0; k<metaElements[i].attributes.length; k++)
+            {
+                if (metaElements[i].attributes[k].name === "property"
+                    && metaElements[i].attributes[k].value === "og:site_name")
+                {
+                    siteName = 1;
+                }
+                else if (metaElements[i].attributes[k].name === "content")
+                {
+                    content = metaElements[i].attributes[k].value;
+                }
+            }
+            
+            if (siteName && content === "Play SRF")
+            {
+                isSrfPlay = 1;
+            }
+        }
+    }
+    
+    if (isSrfPlay)
+    {
+        var nestedRowElement = document.getElementsByClassName("detailPageLeftCol");
+        if (!(nestedRowElement && nestedRowElement.length==1))
+        {
+            nestedRowElement = document.getElementsByClassName("nestedRow");
+        }
+        
+        if (nestedRowElement)
+        {
+            // add the following HTML code to the current document's DOM tree:
+            // <div style="text-align:center">
+            //     <img class="srfchaddon" src="http://addons.cdn.mozilla.net/user-media/addon_icons/413/413748-64.png" />
+            //       Download URLs for this video (right-click)
+            //     <br/>
+            //     <br/>
+            // </div>
+            var downloadImg = document.createElement("img");
+            downloadImg.setAttribute("class", "srfchaddon");
+            downloadImg.setAttribute("src", "http://addons.cdn.mozilla.net/user-media/addon_icons/413/413748-64.png");
+            
+            var downloadA   = document.createElement("a");
+            downloadA.setAttribute("href", document.documentURI);
+            
+            // set text of link
+            var downloadAtext = document.createTextNode(" Download URLs for this video (right-click)");
+            downloadA.appendChild(downloadAtext);
+            
+            var downloadDiv = document.createElement("div");
+            downloadDiv.setAttribute("style", "text-align:center");
+            downloadDiv.appendChild(downloadImg);
+            downloadDiv.appendChild(downloadA);
+            
+            // (two) bracket returns
+            downloadDiv.appendChild(document.createElement('br'));
+            downloadDiv.appendChild(document.createElement('br'));
+            
+            var rowParent = nestedRowElement[0].parentElement;
+            rowParent.insertBefore(downloadDiv, nestedRowElement[0]);
+        }
+    }
+}
